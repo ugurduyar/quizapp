bc78e9c24c682b611d71632577eadbc9c0f6d40a
İsmail Uğur Duyar <ismailugurduyar@gmail.com>
2020-07-19 19:15:59 +0300

    choice selection

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
 
 let currentQuestion = {};
-let acceptingAnswers = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
@@ -64,7 +64,14 @@ getNewQuestion = () => {
 
 choices.forEach((choice) => {
   choice.addEventListener("click", (e) => {
-    console.log(e.target);
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+
+    getNewQuestion();
   });
 });
 
