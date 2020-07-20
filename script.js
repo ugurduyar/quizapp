b8e28013148441c40681efc8ddb72153d2cf0387
İsmail Uğur Duyar <ismailugurduyar@gmail.com>
2020-07-20 16:02:03 +0300

    check answer is correct or incorrect

--- script.js
+++ script.js
@@ -75,6 +75,11 @@ choices.forEach((choice) => {
     const selectedChoice = e.target;
     const selectedAnswer = selectedChoice.dataset["number"];
 
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+    selectedChoice.parentElement.classList.add(classToApply);
+
     getNewQuestion();
   });
 });
