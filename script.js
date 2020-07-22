55630034261159d369677a8dffc73041b90e8090
İsmail Uğur Duyar <ismailugurduyar@gmail.com>
2020-07-22 12:17:09 +0300

    dynamically score increment added

--- script.js
+++ script.js
@@ -81,6 +81,10 @@ choices.forEach((choice) => {
     const classToApply =
       selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
+    if (classToApply === "correct") {
+      incrementScore(CORRECT_BONUS);
+    }
+
     selectedChoice.parentElement.classList.add(classToApply);
 
     setTimeout(() => {
@@ -89,5 +93,10 @@ choices.forEach((choice) => {
     }, 2000);
   });
 });
+
+incrementScore = (num) => {
+  score += num;
+  scoreText.innerText = score;
+};
 
 startGame();
