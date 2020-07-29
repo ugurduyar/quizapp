268d9ff8f8665698ecf71283f7feef4d04313efe
İsmail Uğur Duyar <ismailugurduyar@gmail.com>
2020-07-29 16:09:19 +0300

    save score btn

--- end.js
+++ end.js
@@ -1,5 +1,6 @@
 const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
+
 username.addEventListener("keyup", () => {
   saveScoreBtn.disabled = !username.nodeValue;
 });
