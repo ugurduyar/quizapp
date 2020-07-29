25ab0c089dc4d88819e792085cb7bb9318d90bc5
İsmail Uğur Duyar <ismailugurduyar@gmail.com>
2020-07-29 16:06:56 +0300

    save button is disabled by default

--- end.js
+++ end.js
@@ -1,3 +1,9 @@
+const username = document.getElementById("username");
+const saveScoreBtn = document.getElementById("saveScoreBtn");
+username.addEventListener("keyup", () => {
+  saveScoreBtn.disabled = !username.nodeValue;
+});
+
 saveHighScore = (e) => {
   console.log("save button");
   e.preventDefault();
