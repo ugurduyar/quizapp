62701759dc815e6acc23ff170b86eb2346d33491
İsmail Uğur Duyar <ismailugurduyar@gmail.com>
2020-07-30 14:57:28 +0300

    final score now equals to local storage value

--- end.js
+++ end.js
@@ -1,6 +1,8 @@
 const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
-
+const finalScore = document.getElementById("finalScore");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+finalScore.innerText = mostRecentScore;
 username.addEventListener("keyup", () => {
   saveScoreBtn.disabled = !username.nodeValue;
 });
