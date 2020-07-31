09d4dde73346eeab15ef1a26f80de6cd88eb5914
İsmail Uğur Duyar <ismailugurduyar@gmail.com>
2020-07-31 13:43:17 +0300

    json local storage

--- end.js
+++ end.js
@@ -2,6 +2,10 @@ const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
 const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
+
+localStorage.setItem("highScores", JSON.stringify([]));
+console.log(JSON.parse(localStorage.getItem("highScores")));
+
 finalScore.innerText = mostRecentScore;
 username.addEventListener("keyup", () => {
   saveScoreBtn.disabled = !username.nodeValue;
